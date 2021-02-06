5a59da300b18bb312b66e6436aced42ed2dba44b
Poorvi Vijayvargiya <66432732+poorvi176@users.noreply.github.com>
2021-02-06 22:27:27 +0530

    Create about_us.php

---
+++ about_us.php
@@ -0,0 +1,68 @@
+<?php
+require 'common.php';
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>About Us | Jeevandaan</title>
+        <link rel="shortcut icon" href="img\srtcticon.png" type="image/png">
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial scale=1">
+    </head>
+    
+      
+<body>
+       <!-- Header -->
+    <?php include 'header.php'; ?>
+    <br>
+        <div class="container">
+            <div class="col-sm-4">
+                <div class="panel panel-default">
+                   <div class="panel-body">
+                    <h3 style="color:orange" class="color">WHO WE ARE</h3>
+                    <img src="./img/about-img.jpg" alt="about image"><br><br>
+                    <p>Jeevandaan is a blood bank website that has been made for the purpose of serving the people. It has been made to the help the people those who are in need. Many a times some patients require immediate blood due to some illness or some injury and sometimes its unable to find the right blood. This website right here is made for such emergencies so the people can connect with others in need.<p>
+                </div>
+                </div>
+            </div>
+            
+            
+            <div class="col-sm-4">
+                <div class="panel panel-default">
+                   <div class="panel-body">
+                    <h3 style="color:orange" class="color">BENEFITS OF DONATING BLOOD</h3><br><br>
+                    <h4 style="color:blue">&#45;</h4>
+                    <p> Did you know that people who donate blood are 88% less likely to suffer a heart attack and 33% less likely to acquire any type of cardiovascular disease.</p>
+                    <br>
+                    <h4 style="color:blue">&#45;</h4>
+                    <p> When you donate blood, it removes 225 to 250 milligrams of iron from your body, hence reducing the risk of heart disease.</p>
+                    <br>
+                    <h4 style="color:blue">&#45;</h4>
+                    <p>Blood Center performs numerous tests on the donated blood. Therefore regular blood donation helps in sheilding you from serious diseases.</p>
+                    </div>
+                </div>
+                </div>
+            
+            
+            <div class="col-sm-4">
+                <div class="panel panel-default">
+                   <div class="panel-body">
+                    <h3 style="color:orange" class="color">FACTS ABOUT BLOOD</h3><br><br>
+                        <b></b>
+                            <ol>
+                                <li>You can donate blood every three months. It only takes 48 hours for your body fluids to be completely replenished.</li>
+                                <li>Scientists have estimated the volume of blood in the human body to be eight percent of body weight.</li>
+                                <li>There are 100,000 miles of blood vessels in an adult human body.</li>
+                            </ol>
+                        </div>
+                </div>
+            </div>
+            
+        </div>
+        <?php include 'footer.php'; ?>
+    </body>
+</html>
