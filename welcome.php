bacfaa693fd723ee79131a00c2fac3f4b4f9aa1c
Poorvi Vijayvargiya <66432732+poorvi176@users.noreply.github.com>
2021-02-06 22:24:05 +0530

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,131 @@
+<?php
+require 'common.php';
+
+?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Blood Bank</title>
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial scale=1">
+    </head>
+    <body style='background-image: url(img/background123.jpg); background-repeat: no-repeat; background-size: 1500px 700px '> 
+        
+                <?php
+                include 'header.php';
+                ?>
+    
+    <center>
+                <div id="content">
+            <div id = "banner_image">
+                <div class="inner-banner-image">
+                    
+                        
+                        <div id="banner_content">
+                            <center>
+                            <h1>We Save Lives..</h1>
+                            <a  href="login.php" class="btn btn-danger btn-lg active">Donate Now</a>
+                            </center>
+                        </div>
+                    
+                </div>
+            </div>
+            <br>
+            <br>
+            </div>
+                
+                    </center>
+       
+       
+            <div class="container">
+                <div id="cause">
+                    
+                    
+                    <h1><center>Need for Donation</center></h1>
+                </div>
+                <div class="items">
+                    
+                <div class="panel panel-default">
+                  
+                    <a href="#">
+                        <img src="img/happy-people-patient-aged-man-hospital-physical-clinic-all-smile-help-old-elderly-health-sick-126413384.jpg" alt="" class="thumbnail" width="400" height="300">
+                        <div class="caption">
+                            <h2>Saves lives</h2>
+                            <p>Your blood saves more than one life when it is separated into its components(red blood cells, plasma etc.).It also improves your health</p>
+                        </div>
+                    </a>
+                       </div>
+                    </div>
+                       
+               
+                <div class="items">
+                    
+                <div class="panel panel-default">
+                   <div class="panel-body">
+                    <a href="#">
+                        <img src="img/organ-donation.jpg" alt="" class="thumbnail" width="300" height="200">
+                        <div class="caption">
+                            <h2>ourselves</h2>
+                            <p>Do the good work by donating the blood to the patients that need it regularly with diseases such as hemophilia and thalassemia and also for the imjuries after accidents, surgeries etc.</p>
+                        </div>
+                    </a>
+                       </div>
+                    </div>
+                        </div>
+                
+                <div class="items">
+             
+                <div class="panel panel-default">
+                  
+                    <a href="#">
+                        <img src="img/bigstock-Happy-Senior-People-With-Color-277156504.jpg" alt="" class="thumbnail" width="300" height="220">
+                        <div class="caption">
+                            <h2>Brings happiness</h2>
+                            <p>The most precious gift that one can give to another person is the gift of life i.e. blood. It is the essence of life.</p>
+                        </div>
+                    </a>
+                       
+                    </div>
+                        </div>
+            </div>
+              
+         <!--      <div id="group"> 
+                    <h1>blood group information</h1>
+                    <div class="table-responsive">
+                    <table class="table table-bordered table-hover">
+                        <tbody>
+                            <tr>
+                                <th>blood group</th>
+                                <th>can donate to</th>
+                            </tr>
+                            <tr>
+                                <td>A+</td>
+                                <td>can donate to A+</td>
+                            </tr>
+                            <tr>
+                                <td>O+</td>
+                                <td>can donate to everyone</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                </div> -->
+                
+                <?php
+                include 'footer.php';
+                ?>
+                             
+                        
+                      
+                         
+                            
+            </body>
+</html>
