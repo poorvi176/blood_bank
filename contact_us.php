1befcf22119372d190ab245a969f7a4e0acb3475
Poorvi Vijayvargiya <66432732+poorvi176@users.noreply.github.com>
2021-02-06 22:39:38 +0530

    Create contact_us.php

---
+++ contact_us.php
@@ -0,0 +1,89 @@
+<?php
+include 'common.php';
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Contact Us | Jeevandaan</title>
+        <link rel="shortcut icon" href="img\srtcticon.png" type="image/png">
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial scale=1">
+    </head>
+
+<body style="padding-top: 50px;">
+    
+    <!-- Header -->
+    <?php include 'header.php'; ?>
+    <br>
+    
+<div class="container">
+    <div class="row">
+        <div class="col-sm-10">
+		  <h1 class="title">LIVE SUPPORT</h1>
+		  <h3>24 hours|7 days a week| 365 days a year Live Technical Support</h3><br>
+          <div>
+             <p>The point of using jeevandaan is to get help whenever needed. You can also help anyone by donating your blood.To get in touch with the members of this program for any related query, feel free to drop your query in the below box as suitable.</p>
+          </div>
+      </div>
+        <div class="col-sm-2">
+         <img align="right" src="img/contact.png" alt="contact us">
+        </div>
+    </div>
+    
+
+<div class="row">
+    <div class="col-sm-9">
+        <div class="contact-form">
+            <h2>Get In Touch</h2>
+	           <div class="status alert alert-success" style="display: none"></div>
+                <form id="main-contact-form" class="contact-form row" name="contact-form" method="post" action="sendemail.php">
+				    
+                    <div class="form-group col-sm-9">
+				        <input type="text" name="name" class="form-control" required="required" placeholder="Name" >
+				    </div>
+				    
+                    <div class="form-group col-sm-9">
+				        <input type="email" name="email" class="form-control" required="required" placeholder="Email">
+				    </div>
+				    
+                    <div class="form-group col-sm-9">
+				        <textarea name="message" id="message" required="required" class="form-control" rows="7" placeholder="Your Message Here"></textarea>
+				    </div>
+				            
+                    <div class="form-group col-sm-7">
+				                <input type="submit" name="submit" class="btn btn-primary" value="Submit">
+				    </div>
+				</form>
+        </div>
+    </div>
+	    		
+        <div class="col-sm-3">
+	       <div class="contact-info">
+	           <h2 class="title">Contact Info</h2>
+	               <address>
+				    <p>500 abcd</p>
+				    <p>pqr street,</p>
+				    <p>India</p>
+				    <p>Phone:(00) 222 555 3333</p>
+				    <p>Fax:(000) 222 55 33 6</p>
+				    <p>Email: info@estore.com</p>
+	               </address>
+	               
+	            <div><h2 class="title">Follow Us On&#58;</h2>
+                   <a href="http://www.facebook.com/estore" target="_blank"><img src="./img/logofb.png" alt="fb logo" style="width:30px; height:30px; border:0"></a>
+                   <a href="http://www.twitter.com/estore" target="_blank"><img src="./img/logotwitter.png" alt="twitter logo" style="width:30px; height:30px; border:0 padding-left:10px;"></a>
+               </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+      <?php include 'footer.php'; ?>
+    </body>
+</html>
